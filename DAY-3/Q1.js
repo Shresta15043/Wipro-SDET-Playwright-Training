442208ceebbe3849ae8b2fc077851b88375efac6
Shresta <146806007+Shresta15043@users.noreply.github.com>
2026-05-04 21:19:46 +0530

    forEach

---
+++ DAY-3/Q1.js
@@ -0,0 +1,7 @@
+/*Level 1: The Warm-up (forEach & map)** **1.
+Logging Names** Given an array const names = ['Alice', 'Bob', 'Charlie'];,
+use .forEach() to log each name to the console with the prefix "Hello, ".
+*/
+const names = ['Alice', 'Bob', 'Charlie'];
+
+names.forEach(name => console.log("Hello, " + name));

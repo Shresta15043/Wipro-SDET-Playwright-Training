049ae68701a2040aa7a7a71ea10e8251bf0446af
Shresta <146806007+Shresta15043@users.noreply.github.com>
2026-05-06 09:42:55 +0530

    Async Api

---
+++ DAY-5/AsyncApi.js
@@ -0,0 +1,103 @@
+// =======================
+// EMOJIHUB API
+// =======================
+
+async function getEmojiGroups() {
+  try {
+    const r = await fetch("https://emojihub.yurace.pro/api/groups");
+    const d = await r.json();
+    
+    const arr = Array.isArray(d) ? d : [];
+    
+    const result = arr
+      .filter(g => g.includes('face'))     // keep only groups with "face"
+      .map(g => g.replace('-', ' '));      // "face-positive" -> "face positive"
+    
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+getEmojiGroups();
+
+// =======================
+// RANDOM USER API
+// =======================
+
+async function getRandomUsers() {
+  try {
+    let r = await fetch("https://randomuser.me/api/?results=10");
+    let obj = await r.json();
+
+    let result = obj.results
+      .filter(u => u.gender === "female")
+      .map(u => u.name.first);
+
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+getRandomUsers();
+
+
+// =======================
+//  POKEMON API
+// =======================
+
+async function getPokemon() {
+  try {
+    let r = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20");
+    let obj = await r.json();
+
+    let result = obj.results
+      .filter(p => p.name.startsWith("b"))
+      .map(p => p.name);
+
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+getPokemon();
+
+// =======================
+//  DOG FACTS API
+// =======================
+
+async function getDogFacts() {
+  try {
+    let r = await fetch("https://dogapi.dog/api/v2/facts");
+    let obj = await r.json();
+
+    let result = obj.data
+      .map(d => d.attributes.body);
+
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+getDogFacts();
+
+// =======================
+// CAT FACTS API
+// =======================
+
+async function getCatFacts() {
+  try {
+    let r = await fetch("https://catfact.ninja/facts");
+    let obj = await r.json();
+
+    let result = obj.data
+      .filter(f => f.length < 50)
+      .map(f => f.fact);
+
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+getCatFacts();
+
+

b471a0d6e4a2425442c2ac1bb93f49ab50ebb65b
Shresta <146806007+Shresta15043@users.noreply.github.com>
2026-05-13 15:48:07 +0530

    Task

--- DAY-11/Q1/index.ts
+++ DAY-11/Q1/index.ts
@@ -26,3 +26,52 @@ async function demo() {
 }
 
 demo();
+
+// // 2. Record Mapping for Configuration
+// Scenario: You are managing feature flags or permissions for specific user roles.
+// Task: Define an enum Role { Admin, Editor, Guest }. Use the Record utility type to create a variable PermissionMap where every Role must be mapped to a boolean value. If a role is missing from the object, TypeScript should throw an error.
+
+enum Role {
+  Admin = "Admin",
+  Editor = "Editor",
+  Guest = "Guest"
+}
+
+const PermissionMap: Record<Role, boolean> = {
+  [Role.Admin]: true,
+  [Role.Editor]: true,
+  [Role.Guest]: false
+};
+
+console.log(PermissionMap);
+console.log(`Can Editor edit? ${PermissionMap[Role.Editor]}`);
+
+//  question 3 Exhaustiveness Checking (The never Type)
+// Scenario: You want to ensure that if a new member is added to a Union, your logic must be updated to handle it.
+// Task:
+// Create a union type TaskStatus = 'Open' | 'InProgress' | 'Closed'.
+// Write a function handleTask(status: TaskStatus) using a switch statement.
+// In the default case, assign the status to a variable of type never.
+// The Test: Add 'Archived' to the union and verify that the code fails to compile until you add the new case.
+
+type TaskStatus = 'Open' | 'InProgress' | 'Closed' |'Archived';
+
+function handleTask(status: TaskStatus): string {
+  switch (status) {
+    case 'Open':
+      return 'Task is open';
+    case 'InProgress':
+      return 'Task is in progress';
+    case 'Closed':
+      return 'Task is closed';
+    case 'Archived':
+            return "Task is Archived";  
+    default:
+      // If you miss a case, status here is type 'never'
+      const exhaustiveCheck: never = status;
+      return exhaustiveCheck;
+  }
+}
+
+console.log(handleTask('Open'));
+console.log(handleTask('Closed'));
